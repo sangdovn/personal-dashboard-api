84efc07ef26cbbc252b33b0d4f0862a8a53b17db
Sang Do <dovansang97@gmail.com>
2025-03-18 15:27:09 +0700

    Add isort configuration to pyproject.toml, update GitHub Actions workflow to separate linting and testing jobs, and remove obsolete test file

--- tests/youtube/test_youtube.py
+++
@@ -1,9 +0,0 @@
-from fastapi import status
-
-from tests.utils import client
-
-
-def test_youtube():
-    response = client.get("/youtube")
-    assert response.status_code == status.HTTP_200_OK
-    assert response.json() == {"message": "Hello YouTube"}

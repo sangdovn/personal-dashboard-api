9da95e0b63177da2ca27c5773f6e7690a8e5e2c9
Sang Do <dovansang97@gmail.com>
2025-03-19 01:59:15 +0700

    Remove unused configuration files and YouTube-related modules, and implement a new logging setup in config.py. Update GitHub Actions workflow for development deployment and adjust Docker image tagging for consistency.

--- src/config.py
+++ src/config.py
@@ -3,15 +3,35 @@ from logging.config import dictConfig
 from pydantic import ConfigDict
 from pydantic_settings import BaseSettings
 
-from src.constants import LOGGING_CONFIG
-
 
 def setup_logging():
-    dictConfig(LOGGING_CONFIG)
+    dictConfig(
+        {
+            "version": 1,
+            "disable_existing_loggers": False,
+            "formatters": {
+                "default": {
+                    "format": "%(asctime)s - %(levelname)s - %(filename)s - %(message)s"
+                }
+            },
+            "handlers": {
+                "console": {
+                    "class": "logging.StreamHandler",
+                    "formatter": "default",
+                }
+            },
+            "loggers": {
+                "uvicorn": {"handlers": ["console"], "level": "INFO"},
+                "app_logger": {"handlers": ["console"], "level": "INFO"},
+            },
+        }
+    )
+
+
+setup_logging()
 
 
 class Settings(BaseSettings):
-
     model_config = ConfigDict(env_file=".env")
 
 
--- src/main.py
+++ src/main.py
@@ -1,15 +1,10 @@
+import logging
+
 from fastapi import FastAPI
 
-from src.config import setup_logging
-from src.youtube.router import router as youtube_router
+app = FastAPI()
 
-setup_logging()
 
-app = FastAPI(
-    root_path="/api",
-    docs_url="/docs",
-    redoc_url="/redocs",
-)
-
-# Register routers
-app.include_router(youtube_router)
+@app.get("/home")
+async def index():
+    return {"message": "Hello world!"}

7b33ae1ea1ba05ca54d72de8553cefcfe0e09f3e
Sang Do <dovansang97@gmail.com>
2025-03-19 02:09:28 +0700

    Add logging to the home endpoint in main.py for better request tracking

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
+import logging
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
+logger = logging.getLogger(__name__)
+
 
 @app.get("/home")
 async def index():
+    logger.info("Hello world!")
     return {"message": "Hello world!"}
